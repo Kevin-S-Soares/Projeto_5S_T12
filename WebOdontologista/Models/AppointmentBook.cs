2711837c237f774b370e041f7d4e17494258de59
Kevin S. Soares <kevin.soares@hotmail.com>
2021-11-05 15:39:48 -0300

    Versao 1.1.5. Codigo mais limpo.

--- WebOdontologista/Models/AppointmentBook.cs
+++ WebOdontologista/Models/AppointmentBook.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using WebOdontologista.Models.Interfaces;
 using WebOdontologista.Models.Exceptions;
 using WebOdontologista.Models.CollectionTimePrototype;
-using System.Diagnostics;
 
 namespace WebOdontologista.Models
 {
@@ -14,14 +13,11 @@ namespace WebOdontologista.Models
         private readonly IDentistService _dentistService;
         private readonly ICurrentTimeZoneService _currentTimeZoneService;
 
-        private readonly Dictionary<Dentist, Dictionary<DateTime, ICollectionTimePrototype>> _dentists =
+        private readonly Dictionary<Dentist, Dictionary<DateTime, ICollectionTimePrototype>> _booking =
             new Dictionary<Dentist, Dictionary<DateTime, ICollectionTimePrototype>>();
 
         private readonly Dictionary<Dentist, ICollectionTimePrototype> _prototypeDictionary =
             new Dictionary<Dentist, ICollectionTimePrototype>();
-
-        private readonly Dictionary<Dentist, HashSet<DateTime>> _loadedDentists =
-            new Dictionary<Dentist, HashSet<DateTime>>();
 
         public AppointmentBook(IAppointmentService appointmentService, IDentistService dentistService, ICurrentTimeZoneService currentTimeZoneService)
         {
@@ -31,190 +27,140 @@ namespace WebOdontologista.Models
         }
         public async Task AddAppointment(Appointment appointment)
         {
-            Dentist dentist = await Setup(appointment);
-            if (appointment.DateAndTime() < _currentTimeZoneService.CurrentTime())
-            {
-                throw new DomainException("Data inválida!");
-            }
-            if (_dentists[dentist].ContainsKey(appointment.Date))
-            {
-                _dentists[dentist][appointment.Date].MakeAppointment(appointment);
-            }
-            else
-            {
-                ICollectionTimePrototype clone = _prototypeDictionary[dentist].Clone();
-                clone.MakeAppointment(appointment);
-                _dentists[dentist].Add(appointment.Date, clone);
-            }
+            IsValidDate(appointment.Date);
+            await LoadAppointmentDependecies(appointment);
+            _booking[appointment.Dentist][appointment.Date].MakeAppointment(appointment);
         }
         public async Task RemoveAppointment(int id)
         {
             Appointment appointment = await _appointmentService.FindByIdAsync(id);
-            Dentist dentist = await Setup(appointment);
-            if (_dentists[dentist].ContainsKey(appointment.Date))
-            {
-                _dentists[dentist][appointment.Date].CancelAppointment(appointment);
-            }
-            else
-            {
-                throw new DomainException("Consulta não encontrada!");
-            }
+            await LoadAppointmentDependecies(appointment);
+            _booking[appointment.Dentist][appointment.Date].CancelAppointment(appointment);
+        }
+        public async Task RemoveAppointment(Appointment appointment)
+        {
+            await LoadAppointmentDependecies(appointment);
+            _booking[appointment.Dentist][appointment.Date].CancelAppointment(appointment);
         }
         public async Task EditingAppointment(int id)
         {
-            await RemoveAppointment(id);
+           await RemoveAppointment(id);
         }
         public async Task EditingAppointment(Appointment appointment)
         {
-            Dentist dentist = await Setup(appointment);
-            if (_dentists[dentist].ContainsKey(appointment.Date))
-            {
-                _dentists[dentist][appointment.Date].CancelAppointment(appointment);
-            }
-            else
-            {
-                throw new DomainException("Consulta não encontrada!");
-            }
+            await RemoveAppointment(appointment);
         }
         public async Task EditAppointment(Appointment oldAppointment, Appointment newAppointment)
         {
-            if (newAppointment.DateAndTime() < _currentTimeZoneService.CurrentTime())
-            {
-                throw new DomainException("Data inválida!");
-            }
-            Dentist oldDentist = await Setup(oldAppointment);
-            Dentist newDentist;
-            if (newAppointment.DentistId != oldAppointment.DentistId)
-            {
-                newDentist = await Setup(newAppointment);
-            }
-            else
-            {
-                newDentist = oldDentist;
-            }
-            if (_dentists[oldDentist].ContainsKey(oldAppointment.Date))
-            {
-                _dentists[oldDentist][oldAppointment.Date].CancelAppointment(oldAppointment);
-            }
-            else
-            {
-                throw new DomainException("Consulta não encontrada!");
-            }
-            if (_dentists[newDentist].ContainsKey(newAppointment.Date))
-            {
-                _dentists[newDentist][newAppointment.Date].MakeAppointment(newAppointment);
-            }
-            else
-            {
-                ICollectionTimePrototype clone = _prototypeDictionary[newDentist].Clone();
-                clone.MakeAppointment(newAppointment);
-                _dentists[newDentist].Add(newAppointment.Date, clone);
-            }
+            IsValidDate(newAppointment.Date);
+            await RemoveAppointment(oldAppointment);
+            await AddAppointment(newAppointment);
         }
         public async Task<List<TimeSpan>> FindAvailableTime(Appointment appointment)
         {
-            Dentist dentist = await Setup(appointment);
-            List<TimeSpan> result;
-            if (_dentists.ContainsKey(dentist) && _dentists[dentist].ContainsKey(appointment.Date))
+            await LoadAppointmentDependecies(appointment);
+            List<TimeSpan> result = GetListOfTimesFromPrototypeOrClone(appointment);
+            if(appointment.Date == GetTodayDateOnly())
             {
-                result = _dentists[dentist][appointment.Date].GetAvailableTimes(appointment);
+                RemovePastTimes(result);
             }
-            else
-            {
-                result = _prototypeDictionary[dentist].GetAvailableTimes(appointment);
-            }
-            RemovePastTime(result, appointment);
             return result;
-
         }
-        private async Task<Dentist> Setup(Appointment appointment)
+        private void IsValidDate(DateTime date)
         {
-            Dentist dentist = await AddDentist(appointment);
-            await AddAppointmentsByDentist(dentist, appointment.Date);
-            return dentist;
-        }
-        private async Task<Dentist> AddDentist(Appointment appointment)
-        {
-            Dentist dentist = await _dentistService.FindByIdAsync(appointment.DentistId);
-            if (dentist != null)
+            if (date < GetTodayDateOnly())
             {
-                if (!_dentists.ContainsKey(dentist))
-                {
-                    _dentists.Add(dentist, new Dictionary<DateTime, ICollectionTimePrototype>());
-                    ICollectionTimePrototype prototype;
-                    if (dentist.AppointmentsPerDay() <= 64)
-                    {
-                        prototype = new BitMaskTimePrototype();
-                    }
-                    else
-                    {
-                        prototype = new HashSetTimePrototype();
-                    }
-                    prototype.InstantiateMembers(dentist);
-                    prototype.SetSchedule(dentist);
-                    _prototypeDictionary.Add(dentist, prototype);
-                }
-            }
-            else
-            {
-                throw new DomainException("Odontologista não existe!");
-            }
-            return dentist;
-        }
-        private async Task AddAppointmentsByDentist(Dentist dentist, DateTime date)
-        {
-            if (!_loadedDentists.ContainsKey(dentist) || !_loadedDentists[dentist].Contains(date))
-            {
-                List<Appointment> result = await _appointmentService.FindAllAsync(obj => obj.DentistId == dentist.Id && obj.Date == date);
-                try
-                {
-                    foreach (Appointment obj in result)
-                    {
-                        if (_dentists[dentist].ContainsKey(obj.Date))
-                        {
-                            _dentists[dentist][obj.Date].MakeAppointment(obj);
-                        }
-                        else
-                        {
-                            ICollectionTimePrototype clone = _prototypeDictionary[dentist].Clone();
-                            clone.MakeAppointment(obj);
-                            _dentists[dentist].Add(obj.Date, clone);
-                        }
-                    }
-                }
-                catch (DomainException)
-                {
-                    throw new DomainException("Erro ao carregar os dados!");
-                }
-                if (!_loadedDentists.ContainsKey(dentist))
-                {
-                    _loadedDentists.Add(dentist, new HashSet<DateTime>());
-                }
-                if (!_loadedDentists[dentist].Contains(date))
-                {
-                    _loadedDentists[dentist].Add(date);
-                }
-
+                throw new DomainException("Data inválida!");
             }
         }
-        private void RemovePastTime(List<TimeSpan> list, Appointment appointment)
-        {
-            DateTime today = GetTodayDate();
-            if (appointment.Date == today)
-            {
-                TimeSpan timeNow = _currentTimeZoneService.CurrentTime().TimeOfDay;
-                while (list[0] < timeNow)
-                {
-                    list.RemoveAt(0);
-                }
-            }
-        }
-        private DateTime GetTodayDate()
+        private DateTime GetTodayDateOnly()
         {
             return new DateTime(
                 _currentTimeZoneService.CurrentTime().Year,
                 _currentTimeZoneService.CurrentTime().Month,
-                _currentTimeZoneService.CurrentTime().Day);
+                _currentTimeZoneService.CurrentTime().Day
+                );
+        }
+        public async Task LoadAppointmentDependecies(Appointment appointment)
+        {
+            await SetupDentist(appointment);
+            await SetupAppointments(appointment);
+        }
+        public async Task SetupDentist(Appointment appointment)
+        {
+            if (appointment.Dentist == null)
+            {
+                appointment.Dentist = await _dentistService.FindByIdAsync(appointment.DentistId);
+            }
+            if (!_booking.ContainsKey(appointment.Dentist))
+            {
+                AddDentist(appointment.Dentist);
+                AddDentistPrototype(appointment.Dentist);
+            }
+        }
+        private void AddDentist(Dentist dentist)
+        {
+            _booking.Add(dentist, new Dictionary<DateTime, ICollectionTimePrototype>());
+        }
+        private void AddDentistPrototype(Dentist dentist)
+        {
+            ICollectionTimePrototype prototype;
+            if (dentist.AppointmentsPerDay() <= 64)
+            {
+                prototype = new BitMaskTimePrototype();
+            }
+            else
+            {
+                prototype = new HashSetTimePrototype();
+            }
+            prototype.InstantiateMembers(dentist);
+            prototype.SetSchedule(dentist);
+            _prototypeDictionary.Add(dentist, prototype);
+        }
+        private async Task SetupAppointments(Appointment appointment)
+        {
+            if (!_booking[appointment.Dentist].ContainsKey(appointment.Date))
+            {
+                var list = await _appointmentService.FindAllAsync(obj => obj.DentistId == appointment.DentistId && obj.Date == appointment.Date);
+                ClonePrototypeInADate(appointment);
+                MakeAppointments(list);
+            }
+        }
+
+        private void ClonePrototypeInADate(Appointment appointment)
+        {
+            ICollectionTimePrototype clone = _prototypeDictionary[appointment.Dentist].Clone();
+            _booking[appointment.Dentist].Add(appointment.Date, clone);
+        }
+        private void MakeAppointments(List<Appointment> list)
+        {
+            try
+            {
+                foreach (Appointment obj in list)
+                {
+                    _booking[obj.Dentist][obj.Date].MakeAppointment(obj);
+                }
+            }
+            catch (DomainException)
+            {
+                throw new DomainException("Erro ao carregar os dados!");
+            }
+        }
+        private List<TimeSpan> GetListOfTimesFromPrototypeOrClone(Appointment appointment)
+        {
+            if (_booking.ContainsKey(appointment.Dentist) && _booking[appointment.Dentist].ContainsKey(appointment.Date))
+            {
+                return _booking[appointment.Dentist][appointment.Date].GetAvailableTimes(appointment);
+            }
+            return _prototypeDictionary[appointment.Dentist].GetAvailableTimes(appointment);
+        }
+        private void RemovePastTimes(List<TimeSpan> list)
+        {
+            TimeSpan timeOfTheDay = _currentTimeZoneService.CurrentTime().TimeOfDay;
+            while(list[0] < timeOfTheDay)
+            {
+                list.RemoveAt(0);
+            }
         }
     }
 }
